f60746beb7230300fac642c1341e33983d2aacd1
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-09 10:13:24 -0500

    Saludo mostrado en la misma GUI

--- s01_getting_started/s01_06_saludo.py
+++ s01_getting_started/s01_06_saludo.py
@@ -5,12 +5,16 @@ from flet import ElevatedButton, Page, Text, TextField, Row
 def main(page: Page):
     txt_nombre = TextField(label='Digite su nombre')
 
+    lbl_saludo = Text()
+
     def saludar(event):
-        print(f'¡Hola... {txt_nombre.value}!')
+        lbl_saludo.value = f'¡Hola... {txt_nombre.value}!'
+        page.update()
 
     row = Row(controls=[
         txt_nombre,
-        ElevatedButton(text='Saludar', on_click=saludar)
+        ElevatedButton(text='Saludar', on_click=saludar),
+        lbl_saludo
     ])
 
     page.add(row)
