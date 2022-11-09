e94b338f9591fc5db784f0081217bae8fb7a6bf3
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-09 10:10:38 -0500

    Se muestra mensaje de saludo según el nombre del usuario

--- s01_getting_started/s01_06_saludo.py
+++ s01_getting_started/s01_06_saludo.py
@@ -2,13 +2,14 @@ import flet
 from flet import ElevatedButton, Page, Text, TextField, Row
 
 
-def saludar(event):
-    print('¡Hola!')
-
-
 def main(page: Page):
+    txt_nombre = TextField(label='Digite su nombre')
+
+    def saludar(event):
+        print(f'¡Hola... {txt_nombre.value}!')
+
     row = Row(controls=[
-        TextField(label='Digite su nombre'),
+        txt_nombre,
         ElevatedButton(text='Saludar', on_click=saludar)
     ])
 
