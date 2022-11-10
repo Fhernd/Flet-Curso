38b81e78d704135a34a2edc977ea830dc5fbe036
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-10 12:20:38 -0500

    Demo básica de ElevatedButton - Control de entrada de datos

---
+++ s03_getting_user_input/demo_01_buttons.py
@@ -0,0 +1,12 @@
+import flet
+from flet import ElevatedButton, Page
+
+
+def main(page: Page):
+    btn_accion = ElevatedButton('¡Click!')
+
+    page.add(btn_accion)
+
+
+# Modo Web:
+flet.app(target=main, view=flet.WEB_BROWSER)
