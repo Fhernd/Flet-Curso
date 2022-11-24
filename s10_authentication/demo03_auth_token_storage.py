1038b79eca792f01ffd4056fe6360e43f79ef37f
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-24 10:08:14 -0500

    Instanciación de un proveedor de autenticación

--- s10_authentication/demo03_auth_token_storage.py
+++ s10_authentication/demo03_auth_token_storage.py
@@ -19,4 +19,15 @@ from flet.auth.providers.github_oauth_provider import GitHubOAuthProvider
 from flet.security import decrypt, encrypt
 
 
+def main(page: Page):
+    secret_key = os.getenv('MY_APP_SECRET_KEY')
 
+    provider = GitHubOAuthProvider(
+        client_id=os.getenv('GITHUB_CLIENT_ID'),
+        client_secret=os.getenv('GITHUB_CLIENT_SECRET'),
+        redirect_url='http://localhost:8550/api/oauth/redirect'
+    )
+
+    AUTH_TOKEN_KEY = 'myapp.auth_token'
+
+    
