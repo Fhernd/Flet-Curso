8cef4f0a5b929c2d731d486dd61c969764652741
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-29 16:49:48 -0500

    Demostración de layout para un lector de pantalla

--- s14_accessibility/demo01_screen_reader.py
+++ s14_accessibility/demo01_screen_reader.py
@@ -1,14 +1,38 @@
 import flet
 from flet import FloatingActionButton, KeyboardEvent, Page, Text, icons
-from flet import Main
 
 
 def main(page: Page):
     page.title = "Screen Reader"
-    page.vertical_alignment = flet.MainAxisAlignment.CENTER
-    page.horizontal_alignment = flet.CrossAxisAlignment.CENTER
+    page.vertical_alignment = 'center'
+    page.horizontal_alignment = 'center'
 
     def on_keyboard(event: KeyboardEvent):
         if event.key == 'S' and event.ctrl:
             page.show_semantics_debugger = not page.show_semantics_debugger
             page.update()
+
+    page.on_keyboard_event = on_keyboard
+
+    lbl_numero = Text('0', size=40)
+
+    def btn_incrementar_click(event):
+        lbl_numero.text = str(int(lbl_numero.text) + 1)
+        lbl_numero.update()
+        page.update()
+    
+    btn_incrementar = FloatingActionButton(
+        icon=icons.ADD,
+        on_click=btn_incrementar_click,
+        tooltip='Incrementa valor numérico'
+    )
+
+    page.add(
+        lbl_numero,
+        Text('Presione Ctlr+S para activar el lector de pantalla'),
+        btn_incrementar
+    )
+
+
+if __name__ == '__main__':
+    flet.app(target=main)
