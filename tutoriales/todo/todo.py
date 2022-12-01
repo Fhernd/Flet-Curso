446491f154979fe793a607c785ea08df6c4a3438
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-12-01 15:34:49 -0500

    Campo para escribir el nombre de la tarea y botón para agregarla

--- tutoriales/todo/todo.py
+++ tutoriales/todo/todo.py
@@ -1,11 +1,24 @@
 import flet
-from flet import Page, Text
+from flet import Checkbox, FloatingActionButton, Page, Text, TextField, icons
 
 
 def main(page: Page):
+
+    def btn_agregar_tarea_clicked(event):
+        page.add(
+            Checkbox(label=txt_tarea.value)
+        )
+        txt_tarea.value = ''
+        page.update()
+    
+    txt_tarea = TextField(hint_text='¿Qué necesitas hacer?')
+    btn_agregar_tarea = FloatingActionButton(icon=icons.ADD, on_click=btn_agregar_tarea_clicked)
+
     page.add(
-        Text(value='¡Hola!')
+        txt_tarea,
+        btn_agregar_tarea
     )
+
 
 
 flet.app(target=main)
