86c740691ff1247e08e43c1184a7f3459c63ccd0
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-12-02 15:08:50 -0500

    Encapsulación en una clase para el componente de tareas

--- tutoriales/todo/todo.py
+++ tutoriales/todo/todo.py
@@ -1,38 +1,40 @@
 import flet
-from flet import Checkbox, Column, FloatingActionButton, Page, Row, Text, TextField, icons
+from flet import Checkbox, Column, FloatingActionButton, Page, Row, Text, TextField, UserControl, icons
+
+
+class ToDoApp(UserControl):
+    def build(self):
+        self.txt_tarea = TextField(hint_text='¿Qué necesitas hacer?', expand=True)
+        btn_agregar_tarea = FloatingActionButton(icon=icons.ADD, on_click=self.btn_agregar_tarea_clicked)
+
+        self.col_tareas = Column()
+        col_controles = Column(
+            width=600,
+            controls=[
+                Row(
+                    controls=[
+                        self.txt_tarea,
+                        btn_agregar_tarea
+                    ]
+                ),
+                self.col_tareas
+            ]
+        )
+
+        return col_controles
+    
+    def btn_agregar_tarea_clicked(self, event):
+        self.col_tareas.controls.append(Checkbox(label=self.txt_tarea.value))
+        self.txt_tarea.value = ''
+        self.update()
 
 
 def main(page: Page):
-
-    def btn_agregar_tarea_clicked(event):
-        page.add(
-            Checkbox(label=txt_tarea.value)
-        )
-        txt_tarea.value = ''
-        page.update()
     
-    txt_tarea = TextField(hint_text='¿Qué necesitas hacer?', expand=True)
-    btn_agregar_tarea = FloatingActionButton(icon=icons.ADD, on_click=btn_agregar_tarea_clicked)
-
-    col_tareas = Column()
-    col_controles = Column(
-        width=600,
-        controls=[
-            Row(
-                controls=[
-                    txt_tarea,
-                    btn_agregar_tarea
-                ]
-            ),
-            col_tareas
-        ]
-    )
+    todo = ToDoApp()
 
     page.horizontal_alignment = 'center'
-    page.add(
-        col_controles
-    )
-
+    page.add(todo)
 
 
 flet.app(target=main)
