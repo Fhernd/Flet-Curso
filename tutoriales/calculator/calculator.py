69b5c97165f840105bf9125c5734d0ec953552c0
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-12-03 16:20:39 -0500

    Nuevo layout de organización para las teclas

--- tutoriales/calculator/calculator.py
+++ tutoriales/calculator/calculator.py
@@ -1,32 +1,43 @@
 import flet
-from flet import ElevatedButton, Page, Text
+from flet import ElevatedButton, Page, Row, Text
 
 
 def main(page: Page):
+    page.title = 'Calculadora'
     
     lbl_resultado = Text(value='0')
 
     page.add(
-        lbl_resultado,
-        ElevatedButton(text='AC'),
-        ElevatedButton(text='+/-'),
-        ElevatedButton(text='%'),
-        ElevatedButton(text='/'),
-        ElevatedButton(text='7'),
-        ElevatedButton(text='8'),
-        ElevatedButton(text='9'),
-        ElevatedButton(text='*'),
-        ElevatedButton(text='4'),
-        ElevatedButton(text='5'),
-        ElevatedButton(text='6'),
-        ElevatedButton(text='-'),
-        ElevatedButton(text='1'),
-        ElevatedButton(text='2'),
-        ElevatedButton(text='3'),
-        ElevatedButton(text='+'),
-        ElevatedButton(text='0'),
-        ElevatedButton(text='.'),
-        ElevatedButton(text='='),
+        Row(controls=[lbl_resultado]),
+        Row(controls=[
+            ElevatedButton(text='AC'),
+            ElevatedButton(text='+/-'),
+            ElevatedButton(text='%'),
+            ElevatedButton(text='/')
+        ]),
+        Row(controls=[
+            ElevatedButton(text='7'),
+            ElevatedButton(text='8'),
+            ElevatedButton(text='9'),
+            ElevatedButton(text='*')
+        ]),
+        Row(controls=[
+            ElevatedButton(text='4'),
+            ElevatedButton(text='5'),
+            ElevatedButton(text='6'),
+            ElevatedButton(text='-')
+        ]),
+        Row(controls=[
+            ElevatedButton(text='1'),
+            ElevatedButton(text='2'),
+            ElevatedButton(text='3'),
+            ElevatedButton(text='+')
+        ]),
+        Row(controls=[
+            ElevatedButton(text='0'),
+            ElevatedButton(text='.'),
+            ElevatedButton(text='=')
+        ]),
     )
 
 
