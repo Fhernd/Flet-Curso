2e15cf42dd02438d3794134578cfdc4f0d9be397
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-12-03 16:43:07 -0500

    Contenedor para los controles de la calculadora

--- tutoriales/calculator/calculator.py
+++ tutoriales/calculator/calculator.py
@@ -1,43 +1,53 @@
 import flet
-from flet import ElevatedButton, Page, Row, Text
+from flet import Column, Container, ElevatedButton, Page, Row, Text, border_radius, colors
 
 
 def main(page: Page):
     page.title = 'Calculadora'
     
-    lbl_resultado = Text(value='0')
+    lbl_resultado = Text(value='0', color=colors.WHITE, size=20)
 
     page.add(
-        Row(controls=[lbl_resultado]),
-        Row(controls=[
-            ElevatedButton(text='AC'),
-            ElevatedButton(text='+/-'),
-            ElevatedButton(text='%'),
-            ElevatedButton(text='/')
-        ]),
-        Row(controls=[
-            ElevatedButton(text='7'),
-            ElevatedButton(text='8'),
-            ElevatedButton(text='9'),
-            ElevatedButton(text='*')
-        ]),
-        Row(controls=[
-            ElevatedButton(text='4'),
-            ElevatedButton(text='5'),
-            ElevatedButton(text='6'),
-            ElevatedButton(text='-')
-        ]),
-        Row(controls=[
-            ElevatedButton(text='1'),
-            ElevatedButton(text='2'),
-            ElevatedButton(text='3'),
-            ElevatedButton(text='+')
-        ]),
-        Row(controls=[
-            ElevatedButton(text='0'),
-            ElevatedButton(text='.'),
-            ElevatedButton(text='=')
-        ]),
+        Container(
+            width=300,
+            bgcolor=colors.BLACK,
+            border_radius=border_radius.all(20),
+            padding=20,
+            content=Column(
+                controls=[
+                    Row(controls=[lbl_resultado], alignment='end'),
+                    Row(controls=[
+                        ElevatedButton(text='AC'),
+                        ElevatedButton(text='+/-'),
+                        ElevatedButton(text='%'),
+                        ElevatedButton(text='/')
+                    ]),
+                    Row(controls=[
+                        ElevatedButton(text='7'),
+                        ElevatedButton(text='8'),
+                        ElevatedButton(text='9'),
+                        ElevatedButton(text='*')
+                    ]),
+                    Row(controls=[
+                        ElevatedButton(text='4'),
+                        ElevatedButton(text='5'),
+                        ElevatedButton(text='6'),
+                        ElevatedButton(text='-')
+                    ]),
+                    Row(controls=[
+                        ElevatedButton(text='1'),
+                        ElevatedButton(text='2'),
+                        ElevatedButton(text='3'),
+                        ElevatedButton(text='+')
+                    ]),
+                    Row(controls=[
+                        ElevatedButton(text='0'),
+                        ElevatedButton(text='.'),
+                        ElevatedButton(text='=')
+                    ])
+                ]
+            )
+        )
     )
 
 
