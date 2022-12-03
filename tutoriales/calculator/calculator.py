7579de6f30551e7224df7fbf34bbcf2ad6b5517c
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-12-03 16:08:19 -0500

    Agregadas las teclas de la calculadora

--- tutoriales/calculator/calculator.py
+++ tutoriales/calculator/calculator.py
@@ -1,10 +1,33 @@
 import flet
-from flet import Page, Text
+from flet import ElevatedButton, Page, Text
 
 
 def main(page: Page):
-    page.title = 'Calculator'
-    page.add(Text('Hello World!'))
+    
+    lbl_resultado = Text(value='0')
+
+    page.add(
+        lbl_resultado,
+        ElevatedButton(text='AC'),
+        ElevatedButton(text='+/-'),
+        ElevatedButton(text='%'),
+        ElevatedButton(text='/'),
+        ElevatedButton(text='7'),
+        ElevatedButton(text='8'),
+        ElevatedButton(text='9'),
+        ElevatedButton(text='*'),
+        ElevatedButton(text='4'),
+        ElevatedButton(text='5'),
+        ElevatedButton(text='6'),
+        ElevatedButton(text='-'),
+        ElevatedButton(text='1'),
+        ElevatedButton(text='2'),
+        ElevatedButton(text='3'),
+        ElevatedButton(text='+'),
+        ElevatedButton(text='0'),
+        ElevatedButton(text='.'),
+        ElevatedButton(text='='),
+    )
 
 
 if __name__ == '__main__':
