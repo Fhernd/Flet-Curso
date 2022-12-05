78b98e6589110143f0120e669431e9629bdb31be
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-12-05 16:38:03 -0500

    Asignación de un event handler y datos para cada tecla de la calculadora

--- tutoriales/calculator/calculator.py
+++ tutoriales/calculator/calculator.py
@@ -20,25 +20,33 @@ class CalculadoraApp(UserControl):
                             text='AC',
                             bgcolor=colors.BLUE_GREY_100,
                             color=colors.BLACK,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='AC'
                         ),
                         ElevatedButton(
                             text='+/-',
                             bgcolor=colors.BLUE_GREY_100,
                             color=colors.BLACK,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='+/-'
                         ),
                         ElevatedButton(
                             text='%',
                             bgcolor=colors.BLUE_GREY_100,
                             color=colors.BLACK,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='%'
                         ),
                         ElevatedButton(
                             text='/',
                             bgcolor=colors.ORANGE,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='/'
                         )
                     ]),
                     Row(controls=[
@@ -46,24 +54,32 @@ class CalculadoraApp(UserControl):
                             text='7',
                             bgcolor=colors.WHITE24,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='7'
                         ),
                         ElevatedButton(
                             text='8',
                             bgcolor=colors.WHITE24,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='8'
                         ),
                         ElevatedButton(
                             text='9',
                             bgcolor=colors.WHITE24,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='9'
                         ),
                         ElevatedButton(text='*',
                             bgcolor=colors.ORANGE,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='*'
                         )
                     ]),
                     Row(controls=[
@@ -71,25 +87,33 @@ class CalculadoraApp(UserControl):
                             text='4',
                             bgcolor=colors.WHITE24,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='4'
                         ),
                         ElevatedButton(
                             text='5',
                             bgcolor=colors.WHITE24,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='5'
                         ),
                         ElevatedButton(
                             text='6',
                             bgcolor=colors.WHITE24,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='6'
                         ),
                         ElevatedButton(
                             text='-',
                             bgcolor=colors.ORANGE,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='-'
                         )
                     ]),
                     Row(controls=[
@@ -97,25 +121,33 @@ class CalculadoraApp(UserControl):
                             text='1',
                             bgcolor=colors.WHITE24,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='1'
                         ),
                         ElevatedButton(
                             text='2',
                             bgcolor=colors.WHITE24,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='2'
                         ),
                         ElevatedButton(
                             text='3',
                             bgcolor=colors.WHITE24,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='3'
                         ),
                         ElevatedButton(
                             text='+',
                             bgcolor=colors.ORANGE,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='+'
                         )
                     ]),
                     Row(controls=[
@@ -123,24 +155,40 @@ class CalculadoraApp(UserControl):
                             text='0',
                             bgcolor=colors.WHITE24,
                             color=colors.WHITE,
-                            expand=2
+                            expand=2,
+                            on_click=self.btn_action_clicked,
+                            data='0'
                         ),
                         ElevatedButton(
                             text='.',
                             bgcolor=colors.WHITE24,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='.'
                         ),
                         ElevatedButton(
                             text='=',
                             bgcolor=colors.WHITE24,
                             color=colors.WHITE,
-                            expand=1
+                            expand=1,
+                            on_click=self.btn_action_clicked,
+                            data='='
                         )
                     ])
                 ]
             )
         )
+    
+    def btn_action_clicked(self, event):
+        if event.data == 'AC':
+            self.lbl_resultado.value = '0'
+        elif event.data == '+/-':
+            self.lbl_resultado.value = str(float(self.resultado) * -1)
+        elif event.data == '%':
+            self.lbl_resultado.value = str(float(self.resultado) / 100)
+        elif event.data == '=':
+            self.lbl_resultado.value = ''
 
 
 def main(page: Page):
