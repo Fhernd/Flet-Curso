6165545694b9eae4e577be52853e0622ebc81405
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-12-07 14:37:09 -0500

    Establecer una fuente tipográfica para una aplicación

--- controls/layout/page/demo02_closing.py
+++ controls/layout/page/demo02_closing.py
@@ -1,5 +1,5 @@
 import flet
-from flet import AlertDialog, Page, Text
+from flet import AlertDialog, ElevatedButton, OutlinedButton, Page, Text
 
 
 def main(page: Page):
@@ -13,3 +13,25 @@ def main(page: Page):
     
     page.window_prevent_close = True
     page.on_window_event = window_event
+
+    def yes_clicked(event):
+        page.window_destroy()
+    
+    def no_clicked(event):
+        dlg_confirm.open = False
+        page.update()
+    
+    dlg_confirm = AlertDialog(
+        title=Text('Por favor confirme'),
+        modal=True,
+        content=Text('¿Está seguro que desea salir de la aplicación?'),
+        actions=[
+            ElevatedButton('Sí', on_click=yes_clicked),
+            OutlinedButton('No', on_click=no_clicked)
+        ])
+    
+    page.add(Text('Intente salir de esta aplicación usando el botón de cierre.'))
+
+
+if __name__ == '__main__':
+    flet.app(target=main)
