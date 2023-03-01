58acd6e84072e8d9fdebe665cb6dadf4ff7417fc
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-03-01 15:08:50 -0500

    Barra de aplicación operaciones comunes

--- controls/navigation/app_bar/demo01_app_bar.py
+++ controls/navigation/app_bar/demo01_app_bar.py
@@ -5,8 +5,31 @@ from flet import *
 def main(page: Page):
     def check_item_clicked(event):
         event.control.checked = not event.control.checked
-        page.update()    
+        page.update()
+    
+    page.appbar = AppBar(
+        leading=Icon(icons.PALETTE),
+        leading_width=40,
+        title=Text('AppBar - Ejemplo'),
+        center_title=False,
+        bgcolor=colors.SURFACE_VARIANT,
+        actions=[
+            IconButton(icons.WB_SUNNY_OUTLINED),
+            IconButton(icons.FILTER_3),
+            PopupMenuButton(
+                items=[
+                    PopupMenuItem(text='Item 1'),
+                    PopupMenuItem(),
+                    PopupMenuItem(
+                        text='Checked item', checked=False, on_click=check_item_clicked
+                    ),
+                ]
+            )
+        ]
+    )
+
+    page.add(Text('Contenido de la página'))
 
 
 if __name__ == '__main__':
-    flt.app(target=main)
+    flt.app(target=main, view=flt.WEB_BROWSER)
