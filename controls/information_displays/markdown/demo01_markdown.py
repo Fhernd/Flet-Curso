6ee4d232c671bbda487372379789cb47f43e3872
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-03-04 11:21:25 -0500

    Demostración del control Markdown

---
+++ controls/information_displays/markdown/demo01_markdown.py
@@ -0,0 +1,94 @@
+import flet as flt
+from flet import *
+
+md1 = """
+# Markdown Example
+Markdown allows you to easily include formatted text, images, and even formatted Dart code in your app.
+
+## Titles
+
+Setext-style
+
+This is an H1
+=============
+
+This is an H2
+-------------
+
+Atx-style
+
+# This is an H1
+
+## This is an H2
+
+###### This is an H6
+
+Select the valid headers:
+
+- [x] `# hello`
+- [ ] `#hello`
+
+## Links
+
+[inline-style](https://www.google.com)
+
+## Images
+
+![Image from Flet assets](/icons/icon-192.png)
+
+![Test image](https://picsum.photos/200/300)
+
+## Tables
+
+|Syntax                                 |Result                               |
+|---------------------------------------|-------------------------------------|
+|`*italic 1*`                           |*italic 1*                           |
+|`_italic 2_`                           | _italic 2_                          |
+|`**bold 1**`                           |**bold 1**                           |
+|`__bold 2__`                           |__bold 2__                           |
+|`This is a ~~strikethrough~~`          |This is a ~~strikethrough~~          |
+|`***italic bold 1***`                  |***italic bold 1***                  |
+|`___italic bold 2___`                  |___italic bold 2___                  |
+|`***~~italic bold strikethrough 1~~***`|***~~italic bold strikethrough 1~~***|
+|`~~***italic bold strikethrough 2***~~`|~~***italic bold strikethrough 2***~~|
+
+## Styling
+
+Style text as _italic_, __bold__, ~~strikethrough~~, or `inline code`.
+
+- Use bulleted lists
+- To better clarify
+- Your points
+
+## Code blocks
+
+Formatted Dart code looks really pretty too:
+
+```
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: ft.Markdown(data: markdownData),
+    ),
+  ));
+}
+```
+"""
+
+
+def main(page: Page):
+
+    page.scroll = 'auto'
+
+    md = Markdown(
+        value=md1,  # make its value be equal to the content of our text_field
+        selectable=True,    # to make the rendered markdown selectable
+        extension_set="gitHubWeb",  
+        on_tap_link=lambda e: page.launch_url(e.data),  # what happens when a link is clicked: a browser tab is opened up, with the link's URL 
+    )
+    
+    page.add(md)
+
+
+if __name__ == '__main__':
+    flt.app(target=main, view=flt.WEB_BROWSER)
