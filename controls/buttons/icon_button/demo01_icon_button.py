b37421a9adedf92928c8360dc3c1cd2c3e728a1e
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-03-10 16:21:57 -0500

    Corrección de archivo de código de ejemplo para IconButton

---
+++ controls/buttons/icon_button/demo01_icon_button.py
@@ -0,0 +1,31 @@
+import flet as flt
+from flet import *
+
+
+def main(page: Page):
+    page.title = "Icon Button - Demostración"
+
+    ibn_primero = IconButton(
+        icon=icons.PAUSE_CIRCLE_FILLED_ROUNDED,
+        icon_color='blue400',
+        icon_size=20,
+        tooltip='Pause record'
+    )
+
+    ibn_segundo = IconButton(
+        icon=icons.DELETE_FOREVER_ROUNDED,
+        icon_color='pink600',
+        icon_size=40,
+        tooltip='Delete record'
+    )
+
+    row = Row([
+        ibn_primero,
+        ibn_segundo
+    ])
+
+    page.add(row)
+
+
+if __name__ == '__main__':
+    flt.app(target=main)
---
+++ controls/buttons/icon_button/demo02_icon_button_event.py
@@ -0,0 +1,19 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Icon button with 'click' event"
+
+    def button_clicked(e):
+        b.data += 1
+        t.value = f"Button clicked {b.data} time(s)"
+        page.update()
+
+    b = ft.IconButton(
+        icon=ft.icons.PLAY_CIRCLE_FILL_OUTLINED, on_click=button_clicked, data=0
+    )
+    t = ft.Text()
+
+    page.add(b, t)
+
+ft.app(target=main)
