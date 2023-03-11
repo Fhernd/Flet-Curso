9a629221ade0f18b88fa9073e8999c122689d02e
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-03-11 11:39:37 -0500

    Ejemplo básico de Outlinedbutton

--- controls/buttons/outlined_button/demo01_outlined_button_ejemplos.py
+++ controls/buttons/outlined_button/demo01_outlined_button_ejemplos.py
@@ -1,1 +1,15 @@
+import flet as flt
+from flet import *
 
+
+def main(page: Page):
+    page.title = "Outlined Button - Demostración"
+
+    obn_primero = OutlinedButton(text='Outlined Button')
+    obn_segundo = OutlinedButton('Disabled button', disabled=True)
+
+    page.add(obn_primero, obn_segundo)
+
+
+if __name__ == '__main__':
+    flt.app(target=main)
