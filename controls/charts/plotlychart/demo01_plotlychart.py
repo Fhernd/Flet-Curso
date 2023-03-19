a9826bbae37c5480227968ebd4da0d2dcdb34f71
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-03-19 09:10:52 -0500

    Gráfica con la librería Plotly

--- controls/charts/plotlychart/demo01_plotlychart.py
+++ controls/charts/plotlychart/demo01_plotlychart.py
@@ -1,1 +1,19 @@
+import plotly.express as px
 
+import flet as flt
+from flet import *
+
+from flet.plotly_chart import PlotlyChart
+
+
+def main(page: Page):
+    page.title = "Plotly Chart Demo"
+
+    df = px.data.gapminder().query("continent=='Oceania'")
+    fig = px.line(df, x="year", y="lifeExp", color='country')
+
+    page.add(PlotlyChart(fig, expand=True))
+
+
+if __name__ == "__main__":
+    flt.app(target=main)
