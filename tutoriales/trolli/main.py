4f15625435be5ba6da931a2308c773cc6c49e031
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-03-27 11:15:55 -0500

    Barra de aplicación agregada en la clase TrelloApp

--- tutoriales/trolli/main.py
+++ tutoriales/trolli/main.py
@@ -23,6 +23,26 @@ class TrelloApp:
             PopupMenuItem(text='Settings'),
         ]
 
+        self.appbar = AppBar(
+            leading=Icon(icons.GRID_GOLDENRATIO_ROUNDED),
+            leading_width=100,
+            title=Text('Trulli', size=32, text_align='start'),
+            center_title=False,
+            toolbar_height=75,
+            bgcolor=colors.LIGHT_BLUE_ACCENT_700,
+            actions=[
+                Container(
+                    content=PopupMenuButton(
+                        items=self.appbar_items,
+                    ),
+                    margin=margin.only(left=50, right=25),
+                )
+            ]
+        )
+
+        self.page.appbar = self.appbar
+        self.page.update()
+
 
 if __name__ == '__main__':
 
