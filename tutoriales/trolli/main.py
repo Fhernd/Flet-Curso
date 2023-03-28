1e644c9616ab0ce78bb660a20458742a41d0c186
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-03-28 12:06:53 -0500

    Especificación fuente Pacifico y teme personalizado

--- tutoriales/trolli/main.py
+++ tutoriales/trolli/main.py
@@ -3,6 +3,7 @@ from flet import (
     colors,
     icons,
     margin,
+    Theme,
     AppBar,
     Container,
     Icon,
@@ -58,13 +59,18 @@ if __name__ == '__main__':
 
     def main(page: Page):
         page.title = 'Flet Trello Clone - Trolli'
+        
         page.padding = 0
+        page.theme = Theme(
+            font_family={
+                'Pacifico': '/Pacifico-Regular.ttf'
+            }
+        )
+        page.theme.page_transitions.windows = 'cupertino'
         page.bgcolor = colors.BLUE_GREY_200
 
-        app = TrelloApp(page)
-        page.add(app)
-
         page.update()
+        app = TrelloApp(page)
 
 
-    flet.app(target=main, view=flet.WEB_BROWSER)
+    flet.app(target=main, assets_dir='../assets', view=flet.WEB_BROWSER)
