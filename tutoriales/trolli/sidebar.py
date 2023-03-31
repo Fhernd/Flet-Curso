e8782f3848de6280fee516b36f83591d380f0a08
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-03-31 14:56:36 -0500

    Sincronización de los enlaces de tableros

--- tutoriales/trolli/sidebar.py
+++ tutoriales/trolli/sidebar.py
@@ -12,13 +12,14 @@ from flet import (
     NavigationRailDestination,
     Row,
     Text,
+    TextField,
     UserControl
 )
 
 
 class Sidebar(UserControl):
 
-    def __init__(self, app_layout, page):
+    def __init__(self, app_layout, store: DataStore, page):
         super().__init__()
 
         self.app_layout = app_layout
@@ -91,6 +92,35 @@ class Sidebar(UserControl):
         )
 
         return self.view
+
+    def sync_board_destinations(self):
+        boards = self.store.get_boards()
+        self.bottom_nav_rail.destinations = []
+
+        for i in range(len(boards)):
+            b = boards[i]
+            self.bottom_nav_rail.destinations.append(
+                NavigationRailDestination(
+                    label_content=TextField(
+                        value=b.name,
+                        hint_text=b.name,
+                        text_size=12,
+                        read_only=True,
+                        on_focus=self.board_name_focus,
+                        on_blur=self.board_name_blur,
+                        border='none',
+                        height=50,
+                        width=150,
+                        text_align='start',
+                        data=i
+                    ),
+                    label=boards[i].name,
+                    icon=icons.CHEVRON_RIGHT_OUTLINED,
+                    selected_icon=icons.CHEVRON_RIGHT_ROUNDED,
+                ),
+            )
+        
+        self.view.update()
 
     def top_nav_change(self, event):
         self.top_nav_rail.selected_index = event.control.selected_index
