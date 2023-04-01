00ca1f544478248d4bc4cb267a4d49de5dc80e59
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-04-01 07:50:12 -0500

    Inicio de la codificación del control personalizado que representa un tablero

---
+++ tutoriales/trolli/board.py
@@ -0,0 +1,21 @@
+import itertools
+from flet import (
+    alignment,
+    colors,
+    border,
+    border_radius,
+    icons,
+    margin,
+    padding,
+    AlertDialog,
+    Column,
+    Container,
+    ElevatedButton,
+    FloatingActionButton,
+    Row,
+    Text,
+    TextField,
+    UserControl
+)
+
+
