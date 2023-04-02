537cfab82b709e2db1c8a5fc6292012794ccfe69
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-04-02 10:34:51 -0500

    Constructor o inicializador para un objeto BoardList

--- tutoriales/trolli/board_list.py
+++ tutoriales/trolli/board_list.py
@@ -23,4 +23,27 @@ from flet import (
     UserControl
 )
 
+from tiem import Item
+from data_source import DataSource
 
+class BoardList(UserControl):
+    id_counter = itertools.count()
+
+    def __init__(self, board: 'Board', store: DataSource, title: str, color: str = ''):
+        """
+        Creates a new board list.
+
+        :param board: The board that this list belongs to.
+        :param store: The data store.
+        :param title: The title of the list.
+        :param color: The color of the list.
+        """
+        super().__init__()
+
+        self.board_list_id = next(BoardList.id_counter)
+        self.store: DataSource = store
+        self.board: Board = board
+        self.title = title
+        self.color = color
+        self.items = Column([], tight=True, spacing=4)
+        self.items.controls = self.store.get_items(self.board_list_id)
