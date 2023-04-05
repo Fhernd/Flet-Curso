084aca606dd63937aa87555c60084630d72957dc
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-04-05 08:12:47 -0500

    Inicio importaciones para la clase InMemoryStore

---
+++ tutoriales/trolli/memory_store.py
@@ -0,0 +1,9 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from board import Board
+    from board_list import BoardList
+    from user import User
+    from item import Item
+
+from data_store import DataStore
